acf5e544aa2fba1a854cfa9385fff88a22fd53fc
Alex <146138558+alexanderthegreat24@users.noreply.github.com>
2024-01-31 17:13:55 +0700

    Create Bokings Table

--- schemas.sql
+++ schemas.sql
@@ -6,3 +6,12 @@ CREATE TABLE HOUSES (
         description TEXT,
         host_id INT REFERENCES users(users_id)
 );
+
+CREATE TABLE Bookings (
+        booking_id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES users(user_id),
+        house_id INT REFERENCES houses(house_id),
+        booking_start_date DATE,
+        booking_end_date DATE,
+        price FLOAT
+);
