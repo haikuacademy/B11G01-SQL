c4a6fcd5bfd2e1ccffbd6d26396970b9e146af9b
Chris Vidler <73913511+CSVidler@users.noreply.github.com>
2024-01-31 17:56:51 +0700

    Fixed code errors
    -Made user_id in users PRIMARY KEY
    -fixed users(users_id) -> users(user_id) in houses table
    -Reordered table creation sequence and added number

--- schemas.sql
+++ schemas.sql
@@ -1,4 +1,15 @@
--- Create table houses
+-- Create table 1 (users)
+
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(320) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    profile_pic_url VARCHAR(256)
+);
+
+-- Create table 2 (houses)
 
 CREATE TABLE houses (
         house_id SERIAL PRIMARY KEY NOT NULL,
@@ -6,9 +17,18 @@ CREATE TABLE houses (
         bedrooms INT NOT NULL,
         bathrooms INT NOT NULL,
         description TEXT NOT NULL,
-        host_id INT REFERENCES users(users_id)
+        host_id INT REFERENCES users(user_id)
 );
--- Create table bookings
+
+-- Create table 2b (pictures)
+
+CREATE TABLE pictures(
+    picture_id SERIAL NOT NULL,
+    pic_url VARCHAR(256) NOT NULL,
+    house_id INT NOT NULL REFERENCES houses(house_id)
+);
+
+-- Create table 3 (bookings)
 
 CREATE TABLE bookings (
         booking_id SERIAL PRIMARY KEY NOT NULL,
@@ -19,26 +39,8 @@ CREATE TABLE bookings (
         price FLOAT NOT NULL
 );
 
--- Create table users
 
-CREATE TABLE users(
-    user_id SERIAL NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(320) NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    profile_pic_url VARCHAR(256)
-);
-
--- Create table pictures
-
-CREATE TABLE pictures(
-    picture_id SERIAL NOT NULL,
-    pic_url VARCHAR(256) NOT NULL,
-    house_id INT NOT NULL REFERENCES houses(house_id)
-);
-
--- Create table reviews
+-- Create table 4 (reviews)
 
 CREATE TABLE reviews(
     review_id SERIAL NOT NULL,
