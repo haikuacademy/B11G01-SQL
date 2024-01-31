f67b7bd1a947efc389f93fe51a2a6cf03866d9cb
Chris Vidler <73913511+CSVidler@users.noreply.github.com>
2024-01-31 18:38:21 +0700

    Written queries for user table (+ user data creation fix)
    Created queries (with placeholders) for registering a new user, looking up a user who has requested to login, and updating the user table whenever a user updates their information.
    (Also added commas to fix code to create example users)

--- queries.sql
+++ queries.sql
@@ -1,8 +1,25 @@
 -- User Queries
+-- Creation of data
+INSERT INTO users (first_name, last_name, email, password, profile_pic_url)
+    VALUES
+       ('John', 'Smith', 'jsmith@example.com', '12345', 'jmsmithpic.jpg'),
+       ('Max', 'Mustermann', 'mmustermann@beispiel.com', '54321', 'mmustermannbild.jpg'),
+       ('Ivan', 'Ivanov', 'ivanivanov@primer.com', 'abcde', NULL)
+;
 -- Register (Create)
 INSERT INTO users (first_name, last_name, email, password, profile_pic_url)
     VALUES
-       ('John', 'Smith', 'jsmith@example.com', '12345', 'jmsmithpic.jpg')
-       ('Max', 'Mustermann', 'mmustermann@beispiel.com', '54321', 'mmustermannbild.jpg')
-       ('Ivan', 'Ivanov', 'ivanivanov@primer.com', 'abcde', NULL)
+        (/*New user first name*/, /*New user last name*/,
+        /*New user email*/, /*New user password*/, /*New user profile pic*/)
+;
+-- Login (Read)
+SELECT FROM users (email, password)
+    WHERE (email = /*logginginuseremailaddress@example.com*/
+            AND password = /*logginginuseremailaddress@example.com*/)
+;
+-- Update Profile
+UPDATE users
+    SET profile_pic_url = /*Updated profile pic*/, first_name = /*Updated first name*/, 
+    last_name = /*Updated last name*/, email = /*Updated email address*/
+    WHERE user_id = /*ID No. of user updating information*/
 ;
