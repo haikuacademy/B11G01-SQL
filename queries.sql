2a449535ceb0367658d63ab8482cf1715a550cc4
Chris Vidler <73913511+CSVidler@users.noreply.github.com>
2024-01-31 18:05:34 +0700

    Created example users for users table

---
+++ queries.sql
@@ -0,0 +1,8 @@
+-- User Queries
+-- Register (Create)
+INSERT INTO users (first_name, last_name, email, password, profile_pic_url)
+    VALUES
+       ('John', 'Smith', 'jsmith@example.com', '12345', 'jmsmithpic.jpg')
+       ('Max', 'Mustermann', 'mmustermann@beispiel.com', '54321', 'mmustermannbild.jpg')
+       ('Ivan', 'Ivanov', 'ivanivanov@primer.com', 'abcde', NULL)
+;
