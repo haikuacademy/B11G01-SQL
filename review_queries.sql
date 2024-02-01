016b6178e43c2de94060b1fe34fee5bf4d30a111
Chris Vidler <73913511+CSVidler@users.noreply.github.com>
2024-02-01 14:52:29 +0700

    Fixed SELECT query in review queries file

--- review_queries.sql
+++ review_queries.sql
@@ -6,8 +6,10 @@ INSERT INTO reviews (reviewer_id, house_id, review_text, star_rating, review_dat
         /*Text of review*/, /*Star rating 0-5*/, /*Review date*/)
 ;
 -- Read Review
-SELECT FROM reviews (users.first_name, users.last_name, reviews.review_text, reviews.star_rating, reviews.review_date)
+SELECT users.first_name, users.last_name, 
+reviews.review_text, reviews.star_rating, reviews.review_date
+    FROM reviews
     JOIN users
-    ON reviews.reviwers_id = users.user_id
+    ON reviews.reviwer_id = users.user_id
     WHERE house_id = /*id of house that user wants to see reviews for*/
 ;
