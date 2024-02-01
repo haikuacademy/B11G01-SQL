8d0255fabed6ed399eff8490d65061778691b224
Emiliano <onorati@gmail.com>
2024-02-01 15:53:32 +0700

    fix indent

--- bookings _query.sql
+++ bookings _query.sql
@@ -1,19 +1,14 @@
 SELECT 
-        bookings.house_id, user_id, houses.location, houses.bedrooms, houses.bathrooms, bookings.price, reviews.star_rating 
+  bookings.house_id, user_id, houses.location, houses.bedrooms, houses.bathrooms, bookings.price, reviews.star_rating 
 FROM 
-        bookings 
-FULL JOIN 
-        houses 
+  bookings 
+FULL JOIN houses 
+ON bookings.house_id = houses.house_id
+FULL JOIN reviews 
+ON bookings.house_id = reviews.house_id
+FULL JOIN pictures
 ON 
-        bookings.house_id = houses.house_id
-FULL JOIN
-         reviews 
-ON 
-        bookings.house_id = reviews.house_id
-FULL JOIN
-		pictures
-ON 
-		bookings.house_id = pictures.house_id
+  bookings.house_id = pictures.house_id
 ;
 
 SELECT COUNT(*)
