bf3ae69ac6fea4ddc99e220405d4149448f22b45
Chris Vidler <73913511+CSVidler@users.noreply.github.com>
2024-02-01 15:42:15 +0700

    Updated review queries code to replace placeholder comments with example data

--- review_queries.sql
+++ review_queries.sql
@@ -2,8 +2,7 @@
 -- Create Review
 INSERT INTO reviews (reviewer_id, house_id, review_text, star_rating, review_date)
     VALUES
-        (/*user_id of reviewer*/, /*house_id of reviewed house*/,
-        /*Text of review*/, /*Star rating 0-5*/, /*Review date*/)
+        (2, 1, 'Too much gold everywhere. Incredibly tacky.', 1, 2024-02-01)
 ;
 -- Read Review
 SELECT users.first_name, users.last_name, 
@@ -11,5 +10,5 @@ reviews.review_text, reviews.star_rating, reviews.review_date
     FROM reviews
     JOIN users
     ON reviews.reviwer_id = users.user_id
-    WHERE house_id = /*id of house that user wants to see reviews for*/
+    WHERE house_id = 1
 ;
