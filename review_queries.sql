31565732454e3d983ffb81a317aae4ad6ddfb56f
Emiliano <onorati@gmail.com>
2024-02-01 15:56:54 +0700

    fix typo

--- review_queries.sql
+++ review_queries.sql
@@ -9,6 +9,6 @@ SELECT users.first_name, users.last_name,
 reviews.review_text, reviews.star_rating, reviews.review_date
     FROM reviews
     JOIN users
-    ON reviews.reviwer_id = users.user_id
+    ON reviews.reviewer_id = users.user_id
     WHERE house_id = 1
 ;
