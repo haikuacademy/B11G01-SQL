fe71b5e0da781abf74c02787ec8e4c218cf700c3
Chris Vidler <73913511+CSVidler@users.noreply.github.com>
2024-02-01 14:39:09 +0700

    Improved review queries to return full name
    Improved review queries to return first name and last name of reviewer rather than just user ID of reviewer.

--- review_queries.sql
+++ review_queries.sql
@@ -6,6 +6,8 @@ INSERT INTO reviews (reviewer_id, house_id, review_text, star_rating, review_dat
         /*Text of review*/, /*Star rating 0-5*/, /*Review date*/)
 ;
 -- Read Review
-SELECT FROM reviews (reviewer_id, review_text, star_rating, review_date)
+SELECT FROM reviews (users.first_name, users.last_name, reviews.review_text, reviews.star_rating, reviews.review_date)
+    JOIN users
+    ON reviews.reviwers_id = users.user_id
     WHERE house_id = /*id of house that user wants to see reviews for*/
 ;
