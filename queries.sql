9db843022f5ead53af27d91d3b1e65468c29317a
Alex <146138558+alexanderthegreat24@users.noreply.github.com>
2024-02-01 14:37:52 +0700

    Booking query saved

--- queries.sql
+++ queries.sql
@@ -41,3 +41,24 @@ WHERE house_id = 1
 DELETE 
 FROM houses
 WHERE house_id = 2
+
+
+-- Create Booking
+
+INSERT INTO bookings (user_id, house_id, booking_start_date, booking_end_date, message_to_host)
+VALUES (1, 3, '2024-03-24', '2024-04-01', 'Thanks for the beautiful stay')
+
+-- Read Booking
+
+SELECT 
+        bookings.house_id, user_id, house, houses.location, houses.bedrooms, houses.bathrooms, bookings.price, reviews.star_rating, picture_url 
+FROM 
+        bookings 
+FULL JOIN 
+        houses 
+ON 
+        bookings.house_id = houses.house_id
+FULL JOIN
+         reviews 
+ON 
+        bookings.house_id = reviews.house_id
