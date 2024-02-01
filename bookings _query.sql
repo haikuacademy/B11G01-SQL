71f619b943374f5571a99ae00cbfb30e755f87d6
Alex <146138558+alexanderthegreat24@users.noreply.github.com>
2024-02-01 15:05:38 +0700

    Updatet with additional query on bookings

--- bookings _query.sql
+++ bookings _query.sql
@@ -1,13 +1,5 @@
--- Create Booking
-
-INSERT INTO bookings (user_id, house_id, booking_start_date, booking_end_date, message_to_host)
-VALUES 
-        (1, 3, '2024-03-24', '2024-04-01', 'Thanks for the beautiful stay')
-
--- Read Booking
-
 SELECT 
-        bookings.house_id, user_id, house, houses.location, houses.bedrooms, houses.bathrooms, bookings.price, reviews.star_rating, picture_url 
+        bookings.house_id, user_id, houses.location, houses.bedrooms, houses.bathrooms, bookings.price, reviews.star_rating 
 FROM 
         bookings 
 FULL JOIN 
@@ -18,3 +10,17 @@ FULL JOIN
          reviews 
 ON 
         bookings.house_id = reviews.house_id
+FULL JOIN
+		pictures
+ON 
+		bookings.house_id = pictures.house_id
+;
+
+SELECT COUNT(*)
+FROM reviews
+WHERE reviews.house_id = 2 
+;
+
+SELECT *
+FROM pictures
+WHERE pictures.house_id = 2
